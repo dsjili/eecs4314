a3c8b9469a34026f0559b79249b08cde0d870a78
JulianDeng <dzhongr@my.yorku.ca>
2016-11-14 01:16:19 -0500

    Cleaned up code

--- assignment3_draft/XMLParsing_idea_onlyJava.java
+++ assignment3_draft/XMLParsing_idea_onlyJava.java
@@ -13,48 +13,53 @@ import org.xml.sax.SAXException;
 public class XMLParsing_idea_onlyJava {
 
 	public static void run()  throws ParserConfigurationException, SAXException, IOException {
-		// TODO Auto-generated method stub
+
+		//set read file and write file
 		String fileName = new String("C:\\Users\\dzhon\\Desktop\\Adv. Software Engineering EECS4314\\assignment3\\idea_dependecy.xml");
 		PrintWriter writer = new PrintWriter("idea_dependency_onlyJava.txt", "UTF-8");
 		
-        	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        	DocumentBuilder db = dbf.newDocumentBuilder();
-        	Document document = db.parse(fileName);
-        	NodeList users = document.getChildNodes();   
-       	 
-        	 for(int i = 0; i < users.getLength(); i++){
-        		 Node user = users.item(i);
-        		 NodeList userInfo = user.getChildNodes();
-        		 //System.out.println(users.item(i).getNodeName() + ":");
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(fileName);
+        NodeList users = document.getChildNodes();  
+        
+        //start traversing nodes 
+        for(int i = 0; i < users.getLength(); i++){
+        	Node user = users.item(i);
+        	NodeList userInfo = user.getChildNodes();
         		 
-                 for (int j = 0; j < userInfo.getLength(); j++) {
-                     Node node = userInfo.item(j);
-                     NodeList userMeta = node.getChildNodes();
-                     if(!userInfo.item(j).getNodeName().equals("#text")){                    
-
-                    	 for (int k = 0; k < userMeta.getLength(); k++){
-                    		 if(userMeta.item(k).getNodeName() != "#text"){
-                    			 String dependee = new String(userInfo.item(j).getAttributes().getNamedItem("path").toString());
-                    			 String dependent = new String(userMeta.item(k).getAttributes().getNamedItem("path").toString());
-                    			 
-                    			 dependee = dependee.replace("path=", "");
-                    			 dependee = dependee.replace("\"", "");
-                    			 dependent = dependent.replace("path=", "");
-                    			 dependent = dependent.replace("\"", "");
+            for (int j = 0; j < userInfo.getLength(); j++) {
+                   Node node = userInfo.item(j);
+                   NodeList userMeta = node.getChildNodes();
+                   
+                   if(!userInfo.item(j).getNodeName().equals("#text")){                    
+                	   		for (int k = 0; k < userMeta.getLength(); k++){
+                	   			
+                	   				if(userMeta.item(k).getNodeName() != "#text"){
+                	   						//get dependent and dependee info from path attribute
+                	   						String dependent = new String(userInfo.item(j).getAttributes().getNamedItem("path").toString());
+                	   						String dependee = new String(userMeta.item(k).getAttributes().getNamedItem("path").toString());
+                	   						
+                	   						//formatting the output
+                	   						dependent = dependent.replace("path=", "");
+                	   						dependent = dependent.replace("\"", "");
+                	   						dependee = dependee.replace("path=", "");
+                	   						dependee = dependee.replace("\"", "");
                     			                    			                     			 
-                    			 String output = new String(dependee + "," + dependent);   
-                    			 
-                    			 if(dependee.matches(".+java$") && dependent.matches(".+java$")){
-                    				 System.out.println(output);
-                    				 writer.println(output);   
-                    			 }
+                	   						String output = new String(dependent + "," + dependee);   
+                	   								
+                	   								//output only when dependent and dependee are java files
+                	   								if(dependee.matches(".+java$") && dependent.matches(".+java$")){
+                	   										System.out.println(output);
+                	   										writer.println(output);   
+                	   								}
                     			                  	                   		 
-                    			 }
+                	   				}
 
-                    	 }
-                     }
-                 }
-        	 }
+                	   		}
+                   }
+            }
+       }
         	 
      		writer.flush();
     		writer.close();
